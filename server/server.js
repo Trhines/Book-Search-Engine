6869057af739e5951e8d5a7a8bc5e7648ddfca14
Trhines <trhines94@gmail.com>
2021-09-13 11:26:17 -0700

    moved build

--- server/server.js
+++ server/server.js
@@ -27,9 +27,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+
+  app.use(express.static(path.join(__dirname, 'build')));
+  app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
